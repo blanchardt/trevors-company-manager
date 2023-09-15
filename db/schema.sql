8756451e5864b47141999d5629673f26bc132f2a
unknown <blancardt@merrimack.edu>
2023-09-15 14:41:49 -0400

    added an sql file that joins the tables together.

--- db/schema.sql
+++ db/schema.sql
@@ -22,10 +22,11 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES department(id),
+  REFERENCES department(id)
+  ON DELETE SET NULL,
   FOREIGN KEY (manager_id) 
   REFERENCES employee(id)
   ON DELETE SET NULL
