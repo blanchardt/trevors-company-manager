c954fb60fda80150e69b30ce4e4ff755ae6ee6d4
unknown <blancardt@merrimack.edu>
2023-09-16 13:40:59 -0400

    added the server.js file that creates a connection to a port and to the mysql database.

---
+++ server.js
@@ -0,0 +1,30 @@
+//add the required libraries
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//make connect to the sql server.
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerFDC$88',
+    database: 'company_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
+
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
