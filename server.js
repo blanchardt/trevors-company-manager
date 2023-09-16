83d4ac5a57b1f4650a9a28a01373210cf55936dc
unknown <blancardt@merrimack.edu>
2023-09-16 15:58:21 -0400

    added arrays for the questions that the user will be asked

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//create arrays for the questions and options for users to select select
+var initialResponses = ["View All Employees", "Add Employee", "Update Employee Role", "View All Role", "Add Role", "View All Departments", "Add Department", "Quit"];
+var addingDepartmentQuestion = "What is the name of the department?";
+var addingRoleQuestions = ["What is the name of the role?", "What is the salary of the role?", "Which department does the role belong to?"];
+var addingEmployeeQuestions = ["What is the emlyee's first name?", "What is the emlyee's last name?", "What is the employees role?", "Who is the emplyees's manager?"];
+var managerChoices = ["None"];
+var updateEmployeeQuestions = ["Which employee's role do you want to update?", "Which role do you want to assign the selected employee?"];
+
+
 //make connect to the sql server.
 const db = mysql.createConnection(
   {
@@ -17,7 +26,7 @@ const db = mysql.createConnection(
     password: 'qwerFDC$88',
     database: 'company_db'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the company_db database.`)
 );
 
 
