aeb7cac8e0bb3518fa8a1cc5fca50b68d6310cdf
unknown <blancardt@merrimack.edu>
2023-09-18 15:53:12 -0400

    created a function for adding and viewing all departments.

--- server.js
+++ server.js
@@ -29,6 +29,61 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
+//create a function for view all departments.
+function viewAllDepartments() {
+  db.query('SELECT * FROM department', function (err, results) {
+    var length = 0;
+    var dashes = "";
+    //get the length of the largest department.
+    results.forEach((el) => {
+      if(el.name.length > length) {
+        length = el.name.length;
+      }
+    });
+    //create a - for the largest department name.
+    for (var i = 0; i < length; i++) {
+      dashes += "-";
+    }
+
+    //console log the top of the table.
+    console.log("id  name");
+    console.log(`--  ${dashes}`)
+    
+    //output each element in a loop, have a different output determined by id length.
+    results.forEach((el) => {
+      if (el.id < 10) {
+        console.log(`${el.id}   ${el.name}`);
+      }
+      else {
+        console.log(`${el.id}  ${el.name}`);
+      }
+    });
+    console.log(results);
+  });
+  initialQuestion();
+}
+
+//create a function for adding a department.
+function addDepartment() {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: addingDepartmentQuestion,
+        name: 'department',
+      },
+    ])
+    .then((data) => {
+      db.query(`INSERT INTO department (name) VALUES (?);`, data.department, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      initialQuestion();
+  });
+}
+
+//ask the user what they would like to do , then call another function based off result.
 function initialQuestion() {
   inquirer
   .prompt([
@@ -41,12 +96,30 @@ function initialQuestion() {
   ])
   .then((data) => {
     if(data.request === "View All Employees"){
-        console.log("all emplyees");
+        allEmployees();
+    }
+    else if(data.request === "Add Employee"){
+      addEmployee();
+    }
+    else if(data.request === "Update Employee Role"){
+      updateEmployee();
+    }
+    else if(data.request === "View All Role"){
+      viewAllRoles();
+    }
+    else if(data.request === "Add Role"){
+      addRodle();
+    }
+    else if(data.request === "View All Departments"){
+      viewAllDepartments();
+    }
+    else if(data.request === "Add Department"){
+      addDepartment();
     }
     else {
-        console.log("something else");
+        quit();
     }
-});
+  });
 }
 
 app.use((req, res) => {
